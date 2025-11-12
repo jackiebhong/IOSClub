86e96eea787cb73b905fec8882d72096d4748fd9
zaynahw <zaynahwahab@gmail.com>
2025-11-11 20:57:24 -0500

    folders added + person model (probably not correctly separated yet)

--- iosproject.swiftpm/ContentView.swift
+++ iosproject.swiftpm/ContentView.swift
@@ -9,34 +9,6 @@ import SwiftUI
 let bgColor = Color(red: 0.988, green: 0.988, blue: 0.996)
 let linGradient = Gradient(colors: [Color(red: 0.271, green: 0.337, blue: 0.863), Color(red: 0.455, green: 0.580, blue: 1.00)])
 
-struct Person: Identifiable {
-    
-    var id: UUID = UUID()
-    var name: String
-    var locationMet: String
-    var major: String
-    var dateMet: String
-    var insta: String
-    var tags: [String]
-    var description: String
-    
-    init(name: String = "No Name",
-        locationMet: String = "",
-        major: String = "",
-        dateMet: String = "01/01/2025",
-        insta: String = "",
-        tags: [String] = [],
-         description: String = "")  {
-        self.name = name.isEmpty ? "No Name" : name
-        self.locationMet = locationMet
-        self.major = major
-        self.dateMet = dateMet
-        self.insta = insta
-        self.tags = tags
-        self.description = description.isEmpty ? "No Description": description
-    }
-    
-}
 
 // add
 struct Tag: Identifiable, Hashable {
